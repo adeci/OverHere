8d21e9994656c856d79229e53a8e9f763e8e541d
RoboZero <68201467+RoboZero@users.noreply.github.com>
2023-01-22 01:35:00 -0500

    Finished Return and Handle error section

--- alex-hello/hello.go
+++ alex-hello/hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	greetings "github.com/adeci/OverHere/alex-greetings"
 )
 
 func main() {
-	message := greetings.Hello("Gladys")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
