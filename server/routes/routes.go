3297c936425eb1f0ef95d5d6cbdb72661ee73dad
RoboZero <68201467+RoboZero@users.noreply.github.com>
2023-03-01 20:35:18 -0500

    CORS fix part 2 w/ golang plugin

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,10 +1,25 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateRouter() *gin.Engine {
 	router := gin.Default()
-	router.Use(CORSMiddleware())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	return router
 }
 
