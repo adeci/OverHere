f9c3e6b9468911d70f94595bc3afcefff1193362
RoboZero <68201467+RoboZero@users.noreply.github.com>
2023-03-01 20:36:44 -0500

    CORS part 3, reverting

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,25 +1,12 @@
 package routes
 
 import (
-	"time"
-
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateRouter() *gin.Engine {
 	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://foo.com"},
-		AllowMethods:     []string{"PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
+	router.Use(CORSMiddleware())
 	return router
 }
 
