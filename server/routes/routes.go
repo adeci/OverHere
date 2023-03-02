d41ce8cee3d5206e4d6cd644eedb562842a9ee5f
RoboZero <68201467+RoboZero@users.noreply.github.com>
2023-03-01 20:21:59 -0500

    Adding CORS middleware to stop CORS HTTPS error

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -3,7 +3,9 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func CreateRouter() *gin.Engine {
-	return gin.Default()
+	router := gin.Default()
+	router.Use(CORSMiddleware())
+	return router
 }
 
 func Route(router *gin.Engine) {
@@ -14,4 +16,20 @@ func Route(router *gin.Engine) {
 func Run(router *gin.Engine) {
 	router.SetTrustedProxies([]string{"127.0.0.1"})
 	router.Run("localhost:8000")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
