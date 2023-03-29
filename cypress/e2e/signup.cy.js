d6034a6f699af8e20611f4406b8c7d5073d50310
Ethan Willis <ebwillis123@gmail.com>
2023-03-29 16:42:20 -0400

    new cypress tests

---
+++ cypress/e2e/signup.cy.js
@@ -0,0 +1,27 @@
+describe('template spec', () => {
+  it('runs', () => {
+    cy.visit('localhost:4200/new-user')
+  })
+
+  it('Reroutes on back button press', () => {
+    cy.visit('localhost:4200/new-user')
+    
+    cy.contains('Back').click()
+
+    cy.url().should('include', '/login')
+  })
+
+  it('Recieves inputs and moves on to the next page- e2e', () => {
+    cy.visit('localhost:4200/new-user')
+
+    cy.get('input').type('fakename')
+
+    cy.get('input').type('fakename')
+
+    cy.contains('Confirm').click()
+
+    cy.url().should('include', "/home")
+
+    cy.contains('input')
+  })
+})
