965498616dfaabb424b14538f60eb64a95500e29
nicomaci <nicomacias0303@gmail.com>
2023-03-29 20:15:58 -0400

    updates to http methods

--- server/services/database/database_test.go
+++ server/services/database/database_test.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"testing"
 	"time"
@@ -128,6 +129,29 @@ func TestGetUser_Username(t *testing.T) {
 	}
 }
 
+func TestGetImage_ImageID(t *testing.T) {
+	// Connect
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client := connectMongoDBAtlas()
+	colImages := connectCollection(client, "Images")
+
+	// Upload test
+	var x float64 = 0
+	PostImageBase("please", "", "", "", x, x)
+
+	// Test
+	got, _ := GetImage_ImageID("please")
+	want := createImageObject("please", "", "", "", x, x)
+
+	// Cleanup
+	colImages.DeleteOne(ctx, bson.D{{"imageid", "please"}})
+	client.Disconnect(ctx)
+
+	// Assert
+	fmt.Println(got)
+	fmt.Println(want)
+}
+
 func TestDeleteUser_UserID(t *testing.T) {
 	// Connect
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
@@ -140,6 +164,113 @@ func TestDeleteUser_UserID(t *testing.T) {
 	// Test
 	DeleteUser_UserID("hello7")
 	got, _ := colUsers.CountDocuments(ctx, bson.D{{"userid", "hello7"}})
+	var want int64 = 0
+
+	// Assert
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestDeleteOHPost_OHPostID(t *testing.T) {
+	// Connect
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client := connectMongoDBAtlas()
+	colOHPosts := connectCollection(client, "OHPosts")
+
+	// Upload test
+	PostOHPostBase("TESTTEST", "", "", 0, 0)
+
+	// Test
+	DeleteOHPost_OHPostID("TESTTEST")
+	got, _ := colOHPosts.CountDocuments(ctx, bson.D{{"ohpostid", "TESTTEST"}})
+	var want int64 = 0
+
+	// Assert
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestDeleteOHPost_UserID(t *testing.T) {
+	// Connect
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client := connectMongoDBAtlas()
+	colOHPosts := connectCollection(client, "OHPosts")
+
+	// Upload test
+	PostOHPostBase("", "TESTTEST", "", 0, 0)
+	PostOHPostBase("", "TESTTEST", "", 0, 0)
+	PostOHPostBase("", "TESTTEST", "", 0, 0)
+	PostOHPostBase("", "TESTTEST", "", 0, 0)
+
+	// Test
+	DeleteOHPost_UserID("TESTTEST")
+	got, _ := colOHPosts.CountDocuments(ctx, bson.D{{"ohpostid", "TESTTEST"}})
+	var want int64 = 0
+
+	// Assert
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestDeleteImage_ImageID(t *testing.T) {
+	// Connect
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client := connectMongoDBAtlas()
+	colImages := connectCollection(client, "Images")
+
+	// Upload test
+	PostImageBase("TEST3", "", "", "", 0, 0)
+
+	// Test
+	DeleteImage_ImageID("TEST3")
+	got, _ := colImages.CountDocuments(ctx, bson.D{{"imageid", "TEST3"}})
+	var want int64 = 0
+
+	// Assert
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestDeleteImage_UserID(t *testing.T) {
+	// Connect
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client := connectMongoDBAtlas()
+	colImages := connectCollection(client, "Images")
+
+	// Upload test
+	PostImageBase("", "", "TEST7", "", 0, 0)
+	PostImageBase("", "", "TEST7", "", 0, 0)
+	PostImageBase("", "", "TEST7", "", 0, 0)
+
+	// Test
+	DeleteImage_UserID("TEST7")
+	got, _ := colImages.CountDocuments(ctx, bson.D{{"userid", "TEST7"}})
+	var want int64 = 0
+
+	// Assert
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestDeleteImage_OHPostD(t *testing.T) {
+	// Connect
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client := connectMongoDBAtlas()
+	colImages := connectCollection(client, "Images")
+
+	// Upload test
+	PostImageBase("", "", "", "TEST9", 0, 0)
+	PostImageBase("", "", "", "TEST9", 0, 0)
+	PostImageBase("", "", "", "TEST9", 0, 0)
+
+	// Test
+	DeleteImage_OHPostID("TEST9")
+	got, _ := colImages.CountDocuments(ctx, bson.D{{"ohpostid", "TEST9"}})
 	var want int64 = 0
 
 	// Assert
