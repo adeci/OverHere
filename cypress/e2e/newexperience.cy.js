c2213f6391dc48998aa7c91dddbf22f3ac9dff08
alex <alex.decious@gmail.com>
2023-04-19 22:16:08 -0400

    cypress testing addition

---
+++ cypress/e2e/newexperience.cy.js
@@ -0,0 +1,67 @@
+describe('template spec', () => {
+    it('runs', () => {
+      cy.visit('localhost:4200/new-user')
+    })
+  
+    it('Reroutes on back button press', () => {
+      cy.visit('localhost:4200/new-user')
+      
+      cy.contains('Back').click()
+  
+      cy.url().should('include', '/login')
+    })
+  
+    it('makes new acc and posts then filters only study spot', () => {
+        cy.visit('localhost:4200/new-user')
+
+        cy.get('input').type('fakename')
+
+        cy.contains('Confirm').click()
+
+        cy.url().should('include', "/home")
+
+        cy.contains('fakename')
+
+        cy.contains('View Map/Post to OverHere').click()
+
+        cy.url().should('include', "/map")
+
+        cy.contains('New Post').click()
+
+        cy.get('input[type=text]').type('cypress test post caption!')
+
+        cy.get('select').select('Hangout Spot')
+
+        cy.contains('Submit').click()
+
+        cy.get('#map').click('center')
+    
+        cy.get('.leaflet-marker-icon.leaflet-interactive').click()
+
+        cy.get('.leaflet-touch .leaflet-control-zoom-out').click()
+        cy.get('.leaflet-touch .leaflet-control-zoom-out').click()
+
+        cy.contains('New Post').click()
+
+        cy.get('input[type=text]').type('new second post test!')
+
+        cy.get('select').select('Study Spot')
+    
+        cy.contains('Submit').click()
+    
+        cy.get('#map').click(240, 460)
+    
+        cy.get('#map').click(240, 460)
+    
+        cy.contains('Pin Color Key').click()
+
+        cy.get('button[id="restno"]').click()
+        cy.get('button[id="hangyes"]').click()
+        cy.get('button[id="studyno"]').click()
+        cy.get('button[id="socialno"]').click()
+
+        cy.get('button[id="tagsubmit"]').click()
+    })
+  
+
+  })
